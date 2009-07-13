9c4a6920876df92f5a48263678fc3c32074e5ba3
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-07-13 15:02:54 -0700

    Initial commit

---
+++ eg/track.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use AnyEvent::Twitter::Stream;
+
+my $done = AnyEvent->condvar;
+
+my($user, $password, $method, $args) = @ARGV;
+
+my %args;
+if ($method eq 'follow') {
+    $args{follow} = $args;
+} elsif ($method eq 'track') {
+    $args{track}  = $args;
+}
+
+my $client = AnyEvent::Twitter::Stream->new(
+    username => $user,
+    password => $password,
+    method   => $method || "spritzer",
+    %args,
+    on_tweet => sub {
+        my $tweet = shift;
+        warn "$tweet->{user}{screen_name}: $tweet->{text}\n";
+    },
+    on_error => sub {
+        my $error = shift;
+        warn "ERROR: $error";
+        $done->send;
+    },
+    on_eof   => sub {
+        $done->send;
+    },
+);
+
+$done->recv;
---
+++ lib/AnyEvent/Twitter/Stream.pm
@@ -0,0 +1,125 @@
+package AnyEvent::Twitter::Stream;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+use AnyEvent;
+use AnyEvent::HTTP;
+use JSON;
+use MIME::Base64;
+use URI;
+
+my %methods = map { $_ => 1 } qw( firehose gardenhose spritzer birddog shadow follow track );
+
+sub new {
+    my $class = shift;
+    my %args  = @_;
+
+    my $username = delete $args{username};
+    my $password = delete $args{password};
+    my $method   = delete $args{method};
+    my $on_tweet = delete $args{on_tweet};
+    my $on_error = delete $args{on_error} || sub { die @_ };
+    my $on_eof   = delete $args{on_eof}   || sub {};
+
+    unless ($methods{$method}) {
+        return $on_error->("Method $args{method} not available.");
+    }
+
+    my $auth = MIME::Base64::encode("$username:$password");
+
+    my $uri = URI->new("http://stream.twitter.com/$method.json");
+    $uri->query_form(%args);
+
+    my $handle;
+
+    http_get $uri,
+        headers => { Authorization => "Basic $auth" },
+        on_header => sub {
+            my($headers) = @_;
+            if ($headers->{Status} ne '200') {
+                return $on_error->("$headers->{Status}: $headers->{Reason}");
+            }
+            return 1;
+        },
+        want_body_handle => 1, # for some reason on_body => sub {} doesn't work :/
+        sub {
+            ($handle, my $headers) = @_;
+
+            if ($handle) {
+                $handle->on_eof($on_eof);
+                my $reader; $reader = sub {
+                    my($handle, $json) = @_;
+                    my $tweet = JSON->new->decode($json);
+                    $on_tweet->($tweet);
+                    $handle->push_read(line => $reader);
+                };
+                $handle->push_read(line => $reader);
+            }
+        };
+
+    bless { _handle => $handle }, $class;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+API AnyEvent
+
+=for test_synopsis
+my($user, $password, @following_ids);
+
+=head1 NAME
+
+AnyEvent::Twitter::Stream - Receive Twitter streaming API in an event loop
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Twitter::Stream;
+
+  # receive updates from @following_ids
+  my $client = AnyEvent::Twitter::Stream->new(
+      username => $user,
+      password => $password,
+      method   => "follow",
+      follow   => join(",", @following_ids),
+      on_tweet => sub {
+          my $tweet = shift;
+          warn "$tweet->{user}{screen_name}: $tweet->{text}\n";
+     },
+  );
+
+  # track keywords
+  my $client_keyword = AnyEvent::Twitter::Stream->new(
+      username => $user,
+      password => $password,
+      method   => "track",
+      keyword  => "Perl,Test,Music",
+      on_tweet => sub { },
+  );
+
+=head1 DESCRIPTION
+
+AnyEvent::Twitter::Stream is an AnyEvent user to receive Twitter streaming
+API, available at L<http://apiwiki.twitter.com/Streaming-API-Documentation>
+
+See L<eg/track.pl> for more client code example.
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<AnyEvent::Twitter>, L<Net::Twitter::Stream>
+
+=cut
